f2b0bd6c3b99a9a8ffe58106e5c1ce9a3f9df246
aerthax <aerthax@gmail.com>
2022-05-27 12:54:04 +0200

    debug information

--- weave_delay.js
+++ weave_delay.js
@@ -154,6 +154,7 @@ function drawPlot(x_data, y_data, x_limit, y_limit, svg_id, title) {
 
     let average = (y_data.reduce((a, b) => a + b, 0) / y_data.length) || 0;
     average = average.toFixed(1);
+    console.log(average);
 
     var svg = d3.select(svg_id),
         margin = 100,
